8088cf7566c02a8c0f690bf9a08a01c512a14abc
Deathwing <deathwing@deathwing-Inspiron-N5010.(none)>
2012-12-09 10:27:44 +0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '33a9fbe5b34f35564f440635e67a59eef0a8e76304684d217f276b94bc0ef006c176b0add849e5adcd1e9f8e1ede498fea115b123522745c703f52379da1ed41'
